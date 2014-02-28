9660808c499cf62e907b0f49d415efbb3625982f
Amir Zigangarayev <amirzigangarayev@Mac-mini-Amir-Zigangarayev.local>
2014-02-28 19:41:26 +0400

    Task completed successfully

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,30 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b
+{
+    return a+b;
+}
+
+- (int)divA:(int)a toB:(int)b
+{
+    return a/b;
+}
+
+- (int)sumArray:(NSArray*)arr
+{
+    NSNumber *temp, *result = [NSNumber numberWithInt:0];
+    for (int i = 0; i < [arr count]; i++)
+    {
+        temp = [arr objectAtIndex:i];
+        result = [NSNumber numberWithInt:[temp intValue] + [result intValue]];
+    }
+    return [result intValue];
+}
+
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr
+{
+    return ([self sumArray:firstArr] - [self sumArray:secondArr]);
+}
+
 @end
