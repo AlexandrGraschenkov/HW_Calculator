f7399620dfc03b4d57e7f60ce5ee659f5dd69850
iTumbleweed <ruslan.aiginin@gmail.com>
2014-03-06 02:27:14 +0400

    Implemented functions
    nil

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,31 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b
+{
+    return a + b;
+}
+
+- (int)divA:(int)a toB:(int)b;
+{
+    return a / b;
+}
+
+- (int)sumArray:(NSArray*)arr;
+{
+    int sum = 0;
+    
+    NSArray *newArray=[[NSArray alloc] initWithArray:arr];
+    
+    for (int i = 0; i < arr.count; i++)
+    {
+        sum = sum + [newArray[i] integerValue];
+    }
+    return sum;
+}
+
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr;
+{
+    return [self sumArray:(firstArr)] - [self sumArray:(secondArr)];
+}
 @end
