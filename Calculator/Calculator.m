1be050595ab9ac28cab5244f5e7764f24e68f294
Niki <hanouchh@gmail.com>
2014-03-05 16:10:35 +0400

    Done it !

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,34 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b{
+    
+    return a+b;
+    
+}
+
+- (int)divA:(int)a toB:(int)b{
+    
+    return a/b;
+    
+}
+
+- (int)sumArray:(NSArray*)arr{
+    
+    int result=0;
+    for(NSNumber *digit in arr){
+        result=result+[digit intValue];
+    }
+    return result;
+    
+}
+
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr{
+    
+    int result=0;
+    result = [self sumArray:firstArr]-[self sumArray:secondArr];
+    return result;
+    
+}
+
 @end
