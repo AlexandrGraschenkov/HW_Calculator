bc0e08f8e68b7db050644d89f56345b0608ce7fd
Anastasia Tkachenko <AnastasiaTkachenko@Mac-mini-administrator-75.local>
2014-03-05 07:11:10 -0800

    done

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,33 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b
+{
+    return a+b;
+}
+- (int)divA:(int)a toB:(int)b
+{
+    return  a/b;
+}
+- (int)sumArray:(NSArray*)arr
+{
+    int sum = 0;
+    for (int i=0; i < [arr count]; i++) {
+        sum = sum + [[arr objectAtIndex:i] intValue];
+    }
+    return sum;
+}
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr
+{
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i=0; i < [firstArr count]; i++) {
+        sum1 = sum1 + [[firstArr objectAtIndex:i] intValue];
+    }
+    for (int j=0; j < [secondArr count]; j++) {
+        sum2 = sum2 + [[secondArr objectAtIndex:j] intValue];
+    }
+    return sum1-sum2;
+}
+
 @end
