9d65e7ecfe39bf09f15e0e2a1a2efdcde8c2c5ba
Альбина <aalmukhametova@gmail.com>
2014-03-13 00:24:56 +0400

    Homework done

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,32 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b {
+    return a+b;
+}
+- (int)divA:(int)a toB:(int)b {
+    return a/b;
+}
+- (int)sumArray:(NSArray*)arr {
+    int sum = 0;
+    for (int i = 0; i < [arr count]; i++) {
+        sum += [[arr objectAtIndex:i] integerValue];
+    }
+    return sum;
+}
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr {
+    int sumFirst = 0;
+    int sumSecond = 0;
+    
+    for (int i = 0; i < [firstArr count]; i++) {
+        sumFirst += [[firstArr objectAtIndex:i] integerValue];
+    }
+    
+    for (int i = 0; i < [secondArr count]; i++) {
+        sumSecond += [[secondArr objectAtIndex:i] integerValue];
+    }
+    
+    return sumFirst - sumSecond;
+}
+
 @end
