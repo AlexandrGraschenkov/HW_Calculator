d1de4f412ba70c86f39fe101af3a5382c6d3feba
Айдар <Aidar@Mac-mini-Administrator.local>
2014-03-31 17:17:16 +0400

    Done!

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,55 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b
+{
+    int c = a + b;
+    //    NSLog(@"a %d",a);
+    //    NSLog(@"b %d",b);
+    //    NSLog(@"c %d",c);
+    return c;
+}
+
+- (int)divA:(int)a toB:(int)b
+{
+    int c = a / b;
+    //    NSLog(@"a %d",a);
+    //    NSLog(@"b %d",b);
+    //    NSLog(@"c %d",c);
+    return c;
+}
+
+- (int)sumArray:(NSArray*)arr
+{
+    int a = [arr count];
+    int sum = 0;
+    for(int i = 0; i < a; i++) {
+        NSString *b = [arr objectAtIndex:i];
+//        NSLog(@"%@",b);
+        int q = [b intValue];
+        sum = sum + q;
+    }
+    return sum;
+}
+
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr
+{
+    int a = [firstArr count];
+    int sumA = 0;
+    for(int i = 0; i < a; i++) {
+        NSString *e = [firstArr objectAtIndex:i];
+//        NSLog(@"%@",e);
+        int q = [e intValue];
+        sumA = sumA + q;
+    }
+    int b = [firstArr count];
+    int sumB = 0;
+    for(int i = 0; i < b; i++) {
+        NSString *e = [secondArr objectAtIndex:i];
+//        NSLog(@"%@",e);
+        int q = [e intValue];
+        sumB = sumB + q;
+    }
+    return sumA-sumB;
+}
 @end
