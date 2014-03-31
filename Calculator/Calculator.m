898d5629c298d1788403c4bfdeebc019b9a51ce2
Nelya Zigangarayeva <nelyazigangarayeva@Mac-mini-Amir-Zigangarayev.local>
2014-03-31 14:57:23 +0400

    homework 1 by Zigangaraeva Nelya

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -9,5 +9,24 @@
 #import "Calculator.h"
 
 @implementation Calculator
-
+- (int)sumA:(int)a andB:(int)b
+{
+    return a+b;
+}
+- (int)divA:(int)a toB:(int)b
+{
+    return a/b;
+}
+- (int)sumArray:(NSArray*)arr
+{
+    NSNumber *sum = [[NSNumber alloc] initWithInt:0];
+    for (int i = 0; i< arr.count; i++){
+        sum = [NSNumber numberWithInt:([sum intValue] + [arr[i] intValue])];
+    }
+    return [sum intValue];
+}
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr
+{
+    return [self sumArray: firstArr] - [self sumArray: secondArr];
+}
 @end
