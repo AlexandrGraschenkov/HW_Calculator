8fc6468f69df4f8f2ceba6a9e4604a9fb7dae3b5
itisioslab <itisioslab@Mac-mini-administrator-2.local>
2014-10-14 14:33:16 +0400

    added functions to Calc.m

--- Calculator/main.m
+++ Calculator/main.m
@@ -9,9 +9,18 @@
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
+#import "Calculator.h"
 
 int main(int argc, char * argv[])
 {
+    Calculator *calc = [[Calculator alloc] init];
+    NSArray *a = [[NSArray alloc] initWithObjects:@1, @2, @3, @4, nil];
+    NSArray *b = [[NSArray alloc] initWithObjects:@1, @2, @3, nil];
+    NSLog(@"%d", [calc sumA:10 andB:20]);
+    NSLog(@"%d", [calc divA:30 toB:10]);
+    NSLog(@"%d", [calc sumArray:a]);
+    NSLog(@"%d", [calc subtractSumFirstArray:a withSecondArr:b]);
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
