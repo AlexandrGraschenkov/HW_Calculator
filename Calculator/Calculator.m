6c7aded8c7a79f85ca3a7f1c4cf41116785545e1
BahtiyarovNM <deleted-96@mail.ru>
2014-10-15 18:51:35 +0400

    Domashnee zadanie

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -1,13 +1,45 @@
 //
-//  Calculator.m
-//  Calculator
+// Calculator.m
+// Calculator
 //
-//  Created by Alexander on 27.02.14.
-//  Copyright (c) 2014 Alexander. All rights reserved.
+// Created by Alexander on 27.02.14.
+// Copyright (c) 2014 Alexander. All rights reserved.
 //
 
 #import "Calculator.h"
 
-@implementation Calculator
+Имплементаторы БРС Calculator
+- (int)sumA :(int)a andB :(int)b{
+return a+b;
+}
+- (int)divA:(int)a toB:(int)b{
+return a/b;
+}
+
+- (int)sumArray:(NSArray*)arr{
+
+int count = [arr count];
+NSInteger sum = 0;
+for (int i = 0; i < count; i++) {
+sum += [[arr objectAtIndex:i] integerValue];
+}
+return sum;
+}
+
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr{
+
+int count = [firstArr count];
+NSInteger sum1 = 0;
+for (int i = 0; i < count; i++) {
+sum1 += [[arr objectAtIndex:i] integerValue];
+}
+
+int count = [secondArr count];
+NSInteger sum2 = 0;
+for (int i = 0; i < count; i++) {
+sum2 += [[arr objectAtIndex:i] integerValue];
+}
+return sum1-sum2;
+}
 
 @end
