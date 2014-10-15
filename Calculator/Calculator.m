162713a525aa859c3fdf537f0946620ff50c3802
BahtiyarovNM <deleted-96@mail.ru>
2014-10-15 19:18:32 +0400

    Na bloknote(hardmode)

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -8,38 +8,38 @@
 
 #import "Calculator.h"
 
-Имплементаторы БРС Calculator
+ @implementation Calculator
 - (int)sumA :(int)a andB :(int)b{
-return a+b;
+    return a+b;
 }
 - (int)divA:(int)a toB:(int)b{
-return a/b;
+    return a/b;
 }
 
 - (int)sumArray:(NSArray*)arr{
-
-int count = [arr count];
-NSInteger sum = 0;
-for (int i = 0; i < count; i++) {
-sum += [[arr objectAtIndex:i] integerValue];
-}
-return sum;
+    
+    int count = [arr count];
+    NSInteger sum = 0;
+    for (int i = 0; i < count; i++) {
+        sum += [[arr objectAtIndex:i] integerValue];
+    }
+    return sum;
 }
 
 - (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr{
-
-int count = [firstArr count];
-NSInteger sum1 = 0;
-for (int i = 0; i < count; i++) {
-sum1 += [[arr objectAtIndex:i] integerValue];
-}
-
-int count = [secondArr count];
-NSInteger sum2 = 0;
-for (int i = 0; i < count; i++) {
-sum2 += [[arr objectAtIndex:i] integerValue];
-}
-return sum1-sum2;
+    
+    int count1 = [firstArr count];
+    NSInteger sum1 = 0;
+    for (int i = 0; i < count1; i++) {
+        sum1 += [[firstArr objectAtIndex:i] integerValue];
+    }
+    
+    int count2 = [secondArr count];
+    NSInteger sum2 = 0;
+    for (int i = 0; i < count2; i++) {
+        sum2 += [[secondArr objectAtIndex:i] integerValue];
+    }
+    return sum1-sum2;
 }
 
 @end
