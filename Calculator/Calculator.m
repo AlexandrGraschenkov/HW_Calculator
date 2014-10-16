2db052ccf1c1c622dd69d83e1dd8e9f20b5f1e0d
Airat <mulukov.air@gmail.com>
2014-10-16 21:19:49 +0400

    HW Mulyukov

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,26 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b{
+    return a + b;
+}
+
+- (int)divA:(int)a toB:(int)b{
+    return a/b;
+}
+
+- (int)sumArray:(NSArray*)arr{
+    int result = 0;
+    
+    for (NSNumber *element in arr){
+        result += [element intValue];
+    }
+    
+    return result;
+}
+
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr{
+    return [self sumArray:firstArr] - [self sumArray:secondArr];
+}
+
 @end
