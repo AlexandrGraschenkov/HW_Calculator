ad94e6ae84bf72a05c2b4763cdeb6254523882df
itisioslab <itisioslab@niimm-304c.i.ksu.ru>
2014-11-06 10:35:14 +0300

    Я сделал домашку! :-)

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -9,5 +9,24 @@
 #import "Calculator.h"
 
 @implementation Calculator
+-(int) sumA:(int)a andB:(int)b {
+    return a+b;
+}
+
+-(int) divA:(int)a toB:(int)b {
+    return a/b;
+}
+
+-(int) sumArray:(NSArray *)arr {
+    int result=0;
+    for (NSNumber *element in arr) {
+        result+=[element intValue];
+    }
+    return result;
+}
+
+-(int) subtractSumFirstArray:(NSArray *)firstArr withSecondArr:(NSArray *)secondArr {
+    return [self sumArray:firstArr]-[self sumArray:secondArr];
+}
 
 @end
