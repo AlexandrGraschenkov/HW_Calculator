44e23664b49622af4e9ef2d48c8cd91cbf70ac6d
Bazinko <Zik-zhenya@mail.ru>
2015-02-24 20:10:30 +0300

    added some functions

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,30 @@
 
 @implementation Calculator
 
+- (int)sumA:(int)a andB:(int)b
+{
+    return a + b;
+}
+- (int)divA:(int)a toB:(int)b
+{
+    return a/b;
+}
+- (int)sumArray:(NSArray *)arr
+{
+    NSInteger sum = 0;
+    for(NSNumber *i in arr)
+        sum += [i intValue];
+    return sum;
+}
+- (int)subtractSumFirstArray:(NSArray *)firstArr withSecondArr:(NSArray *)secondArr
+{
+    NSInteger sum1 = 0;
+    NSInteger sum2 = 0;
+    for(NSNumber *i in firstArr)
+        sum1 += [i intValue];
+    for(NSNumber *j in secondArr)
+        sum2 += [j intValue];
+    return sum1 - sum2;
+}
+
 @end
