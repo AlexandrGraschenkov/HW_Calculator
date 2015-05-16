b6a56bb7496065315f0fca24f7e94ef5dfa5a8ec
Admin <admin@Mac-Admin.local>
2015-05-17 02:41:36 +0300

    calc

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -9,5 +9,36 @@
 #import "Calculator.h"
 
 @implementation Calculator
+- (int)sumA:(int)a andB:(int)b
+{
+    return a + b;
+}
+
+- (int)divA:(int)a toB:(int)b
+{
+    return a/b;
+}
+
+- (int)sumArray:(NSArray*)arr
+{
+    NSInteger summ = 0;
+    for(NSNumber *i in arr)
+        summ += [i intValue];
+    return summ;
+}
+
+- (int)subtractSumFirstArray:(NSArray*)firstArr withSecondArr:(NSArray*)secondArr
+{
+    NSInteger fArr = 0;
+    NSInteger sArr = 0;
+    for(NSNumber *i in firstArr)
+        fArr += [i intValue];
+    for (NSNumber *i in secondArr){
+        sArr += [i intValue];
+    }
+    return fArr - sArr;
+    
+    
+}
 
 @end
